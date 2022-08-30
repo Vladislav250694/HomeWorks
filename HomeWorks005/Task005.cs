3d4996ef56b8301e2a8cb4d11b24b2a214b6abbd
vlad <gvm25@bk.ru>
2022-08-30 22:53:57 +0300

    new1

--- HomeWorks005/Task005.cs
+++ HomeWorks005/Task005.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int A=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B=Convert.ToInt32(Console.ReadLine());
+Construction(A,B);
+
+void Construction(int A,int B)
+{
+double Construction=0;
+
+{ 
+    Construction=Math.Pow(A,B);
+    Console.WriteLine ($"Число {A} возведено в степень {B} = {Construction} ");
+}
+}
