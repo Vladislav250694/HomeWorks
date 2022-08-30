1996aad53fb9414452bed209a41204a139bbc532
vlad <gvm25@bk.ru>
2022-08-30 22:55:40 +0300

    new2

--- HomeWorks005/Task005.cs
+++ HomeWorks005/Task005.cs
@@ -4,18 +4,43 @@
 
 // 2, 4 -> 16
 
-Console.Write("Введите число A: ");
-int A=Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
-int B=Convert.ToInt32(Console.ReadLine());
-Construction(A,B);
+// Console.Write("Введите число A: ");
+// int A=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int B=Convert.ToInt32(Console.ReadLine());
+// Construction(A,B);
 
-void Construction(int A,int B)
+// void Construction(int A,int B)
+// {
+// double Construction=0;
+
+// { 
+//     Construction=Math.Pow(A,B);
+//     Console.WriteLine ($"Число {A} возведено в степень {B} = {Construction} ");
+// }
+// }
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {sum(num)}");
+
+int sum(int num)
 {
-double Construction=0;
-
-{ 
-    Construction=Math.Pow(A,B);
-    Console.WriteLine ($"Число {A} возведено в степень {B} = {Construction} ");
-}
-}
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+} 
