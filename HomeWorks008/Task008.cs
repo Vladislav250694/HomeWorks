6bf66d5bcb08fcea934b7cb1a5192fceaac34d6f
vlad <gvm25@bk.ru>
2022-09-19 19:07:32 +0300

    end

--- HomeWorks008/Task008.cs
+++ HomeWorks008/Task008.cs
@@ -78,3 +78,59 @@ void PrintArray1(int[,] array)
         Console.WriteLine(""); 
     }
 }
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+Console.Write ("Введите количество строк массива: ");
+int rows2 = int.Parse (Console.ReadLine());
+Console.Write ("Введите количество столбцов массива: ");
+int colums2 = int.Parse (Console.ReadLine());
+
+int[,] numbers3 = new int[rows2, colums2];
+FillArrayRandomNumbers3(numbers3);
+
+
+for (int j = 0; j < numbers3.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers3.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers3[i, j]);
+    }
+    avarage = avarage / rows2;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArray3(numbers3);
+
+
+
+void FillArrayRandomNumbers3(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray3(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+
