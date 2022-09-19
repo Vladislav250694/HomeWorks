63638b65d25edea0dc1dbb5d8e3eb45c5ff75d1d
vlad <gvm25@bk.ru>
2022-09-19 18:54:20 +0300

    twoTask

--- HomeWorks008/Task008.cs
+++ HomeWorks008/Task008.cs
@@ -33,3 +33,48 @@ void PrintArray(double[,] array)
         Console.WriteLine("");
     }
 }
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.Write ("Введите номер строки: ");
+int rows1 = int.Parse (Console.ReadLine());
+Console.Write ("Введите номер столбца: ");
+int colums1 = int.Parse (Console.ReadLine());
+int [,] numbers1 = new int [4,4];
+FillArrayRandomNumbers1 (numbers1);
+
+if (rows1 > numbers1.GetLength(0) || colums1 > numbers1.GetLength(1))
+{
+    Console.WriteLine("Данного элемент не существует");
+}
+else
+{
+    Console.WriteLine($"значение элемента {rows1}-й строки и {colums1}-го столбца равно {numbers1[rows1-1,colums1-1]}");
+}
+
+PrintArray1(numbers1);
+
+void FillArrayRandomNumbers1(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray1(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
